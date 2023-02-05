f1385dae2a221c28c45296f0c6e2ebe32458c445
forevermgg <centforever@163.com>
2023-02-05 14:27:56 +0800

    find app

--- javascript/watchfile.js
+++ javascript/watchfile.js
@@ -10,12 +10,47 @@
 // Import the filesystem module
 const shell = require('shelljs');
 const fs = require('fs');
+const chokidar = require('chokidar')
+const human = require('human-time')
 console.log(`Process ID: ${process.pid}`);
-process.on('SIGHUP', () => console.log('Received: SIGHUP'));
-process.on('SIGINT', () => console.log('Received: SIGINT'));
-fs.watch("/Users/centforever/AndroidStudioProjects/GradleTest/sdk.json", (eventType, filename) => {
-  console.log("The file", filename, "was modified,The type of change was:", eventType);
+const interval = 500
+// 在按ctrl+c退出进程时也要杀死创建过的子进程
+// 此时需要监听SININT事件,在事件处理函数中杀死子进程并退出主进程。如下所示：
+process.on("SIGINT", () => {
+  console.log('Received: SIGINT')
+  kill(child, () => {
+    process.exit();
+  })
+})
+file="/Users/centforever/AndroidStudioProjects/GradleTest/sdk.json";
+fs.watch(file,{ interval }, (eventType, filename) => {
+  if (eventType == 'change') {
+    console.log("The file", filename, "was modified,The type of change was:", eventType);
+  }
 });
+const watcher = chokidar.watch(file, {
+  alwaysStat: true,
+})
+
+watcher.on('ready', () => {
+  watcher
+    .on('add', (file, stat) => {
+      console.log(`${file} created ${human(stat.birthtime)}`)
+    })
+    .on('unlink', (file) => {
+      console.log(`${file}:removed`)
+    })
+    .on('change', (file, stat) => {
+      const msg = +stat.ctime === +stat.mtime ? 'updated' : 'modified'
+      console.log(`${file}:${msg}:${human(stat.ctime)}`)
+    })
+    .on('addDir', (dir, stat) => {
+      console.log(`${dir} 文件夹created:${human(stat.birthtime)}`)
+    })
+    .on('unlinkDir', (dir) => {
+      console.log(`${dir}:文件夹:removed`)
+    })
+})
 shell.exec("ps aux | grep node | awk '$12~/watchfile.js/{print $2}'", function(code, stdout, stderr) {
   console.log('ps aux | grep node:', code);
   if (code===0) {
