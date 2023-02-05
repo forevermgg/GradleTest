eb35c87946f16520d6ac84f2de51fd9db73ffb71
forevermgg <centforever@163.com>
2023-02-05 13:51:09 +0800

    find app

--- javascript/watchfile.js
+++ javascript/watchfile.js
@@ -8,10 +8,32 @@
 // killall -9 node
 // ps aux | grep node | awk '$12~/watchfile.js/{print $2}'
 // Import the filesystem module
+const shell = require('shelljs');
 const fs = require('fs');
 console.log(`Process ID: ${process.pid}`);
 process.on('SIGHUP', () => console.log('Received: SIGHUP'));
 process.on('SIGINT', () => console.log('Received: SIGINT'));
 fs.watch("/Users/centforever/AndroidStudioProjects/GradleTest/sdk.json", (eventType, filename) => {
   console.log("The file", filename, "was modified,The type of change was:", eventType);
+});
+shell.exec("ps aux | grep node | awk '$12~/watchfile.js/{print $2}'", function(code, stdout, stderr) {
+  console.log('ps aux | grep node:', code);
+  if (code===0) {
+    console.log('ps aux | grep node program output:', stdout);
+    node_pids = stdout
+    node_pids.split(/\r?\n/).forEach(line =>  {
+      if (line != "") {
+        console.log(`Line from file: ${line}`);
+      }
+
+      if (line != "" && line != process.pid) {
+        console.log(`kill -9 ${line}`);
+        shell.exec(`kill -9 ${line}`)
+      }
+    });
+    console.log('ps aux | grep node 成功')
+  } else {
+    console.log('ps aux | grep node program stderr:', stderr);
+    console.log('ps aux | grep node 失败')
+  }
 });
