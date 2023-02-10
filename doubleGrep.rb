2cfb0ca3d2f3dd54a6b3f70398d435459768f018
forevermgg <centforever@163.com>
2023-02-11 00:55:25 +0800

    readme

---
+++ doubleGrep.rb
@@ -0,0 +1,5 @@
+# 终端下双重过滤筛选内容
+# https://droidyue.com/blog/2020/10/20/double-find-content/
+# doubleGrep.rb ./ ruby Android
+#!/usr/bin/env ruby
+# encoding: utf-8
---
+++ shell/checkmac.sh
@@ -0,0 +1,10 @@
+# https://droidyue.com/blog/2021/05/07/mac-check-login-reboot-shutdown-log/
+# Mac 查看系统的在线，登陆，重启，关机日志时间
+# 查看运行时间
+uptime
+# 最近的重启时间点
+last reboot
+# 最近的关机时间点
+last shutdown
+# 最近的登陆时间点
+last forevermgg
